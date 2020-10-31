760983477a3a21ead56d0ed26b4a062a98a7390e
Dirk Kelderman <dirkkelderman@gmail.com>
2020-10-31 10:53:09 +0100

    restored version, updated files

--- canvas.js
+++ canvas.js
@@ -11,14 +11,14 @@ const myGameArea = {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
   score: function () {
-    this.ctx.font = '40px arial';
+    this.ctx.font = '40px Punkboy';
     this.ctx.fillStyle = 'white';
     this.ctx.fillText(`Score: ${game.score}`, 140, 100);
   },
   attempts: function () {
-    this.ctx.font = '40px arial';
+    this.ctx.font = '40px Punkboy';
     this.ctx.fillStyle = 'white';
-    this.ctx.fillText(`Attempts: ${game.attempts}`, 350, 100);
+    this.ctx.fillText(`Attempts: ${game.attempts}`, 700, 100);
   },
   stadium: function () {
     let stadiumImg = new Image();
@@ -66,71 +66,12 @@ function updateGameArea() {
   myGameArea.drawPointer();
   game.pointer.newPos();
   game.pointer.update();
-  // requestAnimationFrame(animate);
-  // drawShootButton()
 }
 
-myGameArea.start();
+ 
 
 function startGame() {
   setInterval(updateGameArea, 20);
   
 }
-
-startGame()
-
-
-// function drawPointer () {
-//   let PointerImg = new Image();
-//   PointerImg.src = './img/crosshairgreen.png';
-//   myGameArea.ctx.drawImage(PointerImg, game.pointer.x, game.pointer.y, 50, 50);
-// };
-  // function ballShoot () {
-  //   let ballImg = new Image();
-  //   ballImg.src = './img/football.png';
-  //   myGameArea.ctx.drawImage(ballImg, game.ball.x, game.ball.y, game.ball.width, game.ball.height)
-  // };
-
-
-// const canvas = document.getElementById('canvas');
-// const ctx = canvas.getContext('2d');
-
-// myGameArea.ctx.fillStyle = '#FF0000';
-// // ctx.fillRect(100, 0, 50, 50);
-// // ctx.fillRect(300, 0, 50, 50);
-// // ctx.fillRect(500, 0, 50, 50);
-
-// let speed1 = 490;
-
-
-// // function clearCanvas() {
-// //   myGameArea.ctx.clearRect(0, 0, 700, 450); // 700 and 450 are canvas width and height
-// // }
-
-// function ballShoot(x, y, w, h, color) {
-  
-//   myGameArea.ctx.fillStyle = color;
-//   myGameArea.ctx.fillRect(x, y, w, h);
-// }
-
-// function updateCanvas() {
-//   // in order to see animation, let's change speed1,2 and 3 on every call
-//   speed1 -= 5;
-
-//   // clear the canvas
-//   startGame()
-
-//   // redraw the canvas
-//   ballShoot(487.5, speed1, 50, 50, 'red');
-
-//   if (speed1 === 160) {
-//     myGameArea.ctx.cancelAnimationFrame;
-//   } else {
-//     requestAnimationFrame(updateCanvas);
-
-//   }
-//   // requestAnimationFrame(updateCanvas);
-// }
-
-// updateCanvas()
 
